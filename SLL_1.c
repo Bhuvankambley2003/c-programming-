cd14c4620718210d745f41a0d41a53734e72c4fc
bhuvan <pbhuvankambley2003@gmail.com>
2022-12-11 13:28:29 +0530

    SLL1_.c

---
+++ SLL_1.c
@@ -0,0 +1,153 @@
+#include<stdlib.h>
+#include <stdio.h>
+     
+void create();
+void display();
+//void insert_begin();
+void insert();
+//void insert_pos();
+void delete();
+//void delete_end();
+//void delete_pos();
+ 
+ 
+struct node
+{
+        int info;
+        struct node *next;
+};
+struct node *start=NULL;
+int main()     
+{
+        int choice;
+        while(1){
+               
+                printf("\n                MENU                             \n");
+                printf("\n 1.Create     \n");
+                printf("\n 2.Display    \n");
+                printf("\n 3.Insert     \n");
+                printf("\n 4.Delete     \n");
+                printf("\n 5.Exit       \n");
+                printf("\n--------------------------------------\n");
+                printf("Enter your choice:\t");
+                scanf("%d",&choice);
+                switch(choice)
+                {
+                        case 1:
+                                        create();
+                                        break;
+                        case 2:
+                                        display();
+                                        break;
+                        
+                                        break;
+                        case 3:
+                                        insert();
+                                        break;
+                        
+                        case 4:
+                                        delete();
+                                        break;
+                        
+                       
+                        
+                        case 5:
+                                        exit(0);
+                                        break;
+                             
+                        default:
+                                        printf("n Wrong Choice:n");
+                                        break;
+                }
+        }
+        return 0;
+}
+void create()
+{
+        struct node *temp,*ptr;
+        temp=(struct node *)malloc(sizeof(struct node));
+        if(temp==NULL)
+        {
+                printf("\nOut of Memory Space:\n");
+                exit(0);
+        }
+        printf("\nEnter the data value for the node:\t");
+        scanf("%d",&temp->info);
+        temp->next=NULL;
+        if(start==NULL)
+        {
+                start=temp;
+        }
+        else
+        {
+                ptr=start;
+                while(ptr->next!=NULL)
+                {
+                        ptr=ptr->next;
+                }
+                ptr->next=temp;
+        }
+}
+void display()
+{
+        struct node *ptr;
+        if(start==NULL)
+        {
+                printf("\nList is empty:\n");
+                return;
+        }
+        else
+        {
+                ptr=start;
+                printf("\nThe List elements are:\n");
+                while(ptr!=NULL)
+                {
+                        printf("%d\t",ptr->info );
+                        ptr=ptr->next ;
+                }
+        }
+}
+
+void insert()
+{
+        struct node *temp,*ptr;
+        temp=(struct node *)malloc(sizeof(struct node));
+        if(temp==NULL)
+        {
+                printf("\nOut of Memory Space:\n");
+                return;
+        }
+        printf("\nEnter the data value for the node:\t" );
+        scanf("%d",&temp->info );
+        temp->next =NULL;
+        if(start==NULL)
+        {
+                start=temp;
+        }
+        else
+        {
+                ptr=start;
+                while(ptr->next !=NULL)
+                {
+                        ptr=ptr->next ;
+                }
+                ptr->next =temp;
+        }
+}
+
+void delete()
+{
+        struct node *ptr;
+        if(ptr==NULL)
+        {
+                printf("\nList is Empty:\n");
+                return;
+        }
+        else
+        {
+                ptr=start;
+                start=start->next ;
+                printf("\nThe deleted element is :%d\t",ptr->info);
+                free(ptr);
+        }
+}
